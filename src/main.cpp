51301794ec29f971d27bed25ef2c1a0a84940911
BaGreal2 <artemonik17@gmail.com>
2023-11-21 23:37:20 +0100

    remove: .DS_Store

--- src/main.cpp
+++ src/main.cpp
@@ -86,6 +86,9 @@ int main() {
       }
     }
 
+    graphics.draw(someText);
+    graphics.draw(someBoldText);
+
     graphics.end();
 
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
